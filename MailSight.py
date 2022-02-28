3ec1b54bb33a0250341bf2dc22e253b1880fa84e
jws8 <94502439+jws8@users.noreply.github.com>
2022-02-27 23:35:23 -0800

    new total library

--- MailSight.py
+++ MailSight.py
@@ -1,18 +1,27 @@
 #Email reader, sender, searcher, to attach to XLLibaries
-#Author: Joshua Wilson Smith (https://www.github.com/jws8)
+#Author: Joshua A.W. Smith (https://www.github.com/jws8)
 #Date: 1/26/2022
-import smtplib, email, ssl, imaplib, pwinput
+import smtplib, imaplib, email, ssl, pwinput
+from types import NoneType
 #Must allow 3rd party "less-secure" applications to run in gmail settings
 #: THIS IS NOT THE RECOMMENDED WAY OF LOGGING IN... USE OS 2/16/22
 #A later version of MailSight will automate the local OS environment 
 #but I'm a bit fuzzy, currently, on how to do this globally. 2/16/22 
 class MailSight():
     def __init__(self):
+        #to make the program dynamic use these username and pw vars
         #self.pw_input = pwinput.pwinput()
-        self.username = str(input("enter username: "))
-        self.password = pwinput.pwinput(mask = "$")
+        #self.username = str(input("enter username: "))
+        #self.password = pwinput.pwinput(mask = "$")
+        #use these for automation
+        self.username = ""
+        self.password = ""
         self.subject = ""
         self.body = ""
+        self.RFC_ID = "(RFC822)"
+        self.raw = NoneType
+        self.selection = "inbox"
+        self.user = NoneType
         #smtp protocol
         self.smtp_url = "smtp.gmail.com"
         #imap-->gmail protocol
@@ -23,26 +32,43 @@ class MailSight():
         self.context = ssl.create_default_context()
         #dynamic program: self.receiving_email = str(input("where would you like to send this to?"))
         self.receiver_list = []
+        self.inbox_item_list = []
     #verify that program is initialized
     print("Starting program...")
 
-    def read_mail(self):
-        print("reading mail")
+    def app_login(self, username, pw):
+        self.username = username
+        self.password = pw
+        print("LOGGED IN")
+    
+    def app_logout(self):
+        self.user.logout()
+        print("LOGGED OUT")
+
+    def read_mail(self, selection):
+        self.selection = selection
+        print(f"Selection: {selection}")
         #create a user object
-        user = imaplib.IMAP4_SSL(self.imap_url)
-        print("logging in")
+        self.user = imaplib.IMAP4_SSL(self.imap_url)
+        print("user defined...")
         #login
-        user.login(self.username, self.password)
+        self.user.login(self.username, self.password)
         #select imap object: inbox
-        user.select("inbox")
-        user.list() #?
-        t, data = user.uid("search", None, "ALL")
-        inbox_item_list = data[0].split()
-        #get most recent
-        most_recent = inbox_item_list[-1]
+        self.user.select(self.selection)
+        self.user.list() #?
+        t, all_data = self.user.uid("search", None, "ALL")
+        self.inbox_item_list = all_data[0].split()
+        print(self.inbox_item_list)
+    #Not a working method
+    def get_abit_data(self, b_num):
+        t, data = self.user.uid("fetch", b"1", self.RFC_ID)
+        print(data)
         
-        result, email_data = user.uid("fetch", most_recent, "(RFC822)")
-        print(email_data)
+    def get_most_recent(self): #get most recent
+        most_recent = self.inbox_item_list[-1]
+        result, email_data = self.user.uid("fetch", most_recent, "(RFC822)")
+        self.raw = email.message_from_bytes(email_data[0][1])
+        print(self.raw)
         print("done")
     
     #params STR:subject_str, STR:message_str, LIST: address_list
@@ -53,7 +79,7 @@ class MailSight():
                 #1 hour later: registered meaning is defined as security error, not pw error #1//27/22 10:08PM 
                 user.login(self.username, self.password)
             except smtplib.SMTPAuthenticationError: 
-                print("\n IMPORTANT: \nYou need to change Gmail settings to allow \"less secure apps\"\n")
+                print("\n IMPORTANT: \nYou need to change Gmail settings to allow \"less secure apps\"\n OR make sure your script uses the app_login method")
             self.receiver_list = address_list #change 2/16/22
             #passed in params
             subject = subject_str
@@ -61,8 +87,18 @@ class MailSight():
             message = f"Subject: {subject}\n\n{body}"
             user.sendmail(self.username, self.receiver_list, message)
         print("sent email!")
-    
+
 #Run Template
 #my_gmail = MailSight()
-#my_gmail.send_mail("Subject:", "Body:", email_list)
+#my_gmail.app_login("YourUserName", "YourPassword")
+#email_list = ["johndoe@gmail.com, "marymartinez@gmail.com"]
+#read mail example
+#my_gmail.read_mail("inbox") 
+#my_gmail.get_most_recent()
+#my_gmail.app_logout() 
+#send mail examples 
+#my_gmail.send_mail("Subject", "Body", email_list)
+#i = 0
+#for i in range(len(email_list)):
+ #   my_gmail.send_mail(email_list[i], my_gmail.raw, email_list[i])
 
